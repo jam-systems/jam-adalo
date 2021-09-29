70a2aa90b45da8186a982ff7bfd0e1eb3c96bb97
Gregor <gregor.mitscha-baude@gmx.at>
2021-09-29 17:39:33 +0200

    start with native component

--- jam/src/components/JamComponent/index.js
+++ jam/src/components/JamComponent/index.js
@@ -1,16 +1,49 @@
-import React, { Component } from 'react'
-import { View } from 'react-native'
+import React, { Component, StyleSheet } from "react";
+import { View } from "react-native";
 
 class JamComponent extends Component {
+  render() {
+    console.log("editor");
+    console.log(this.props.editor);
+    console.log(this.props);
 
-	render() {
+    let iFrameHash = base64.encodeUrl(
+      JSON.stringify({
+        room: {
+          stageOnly: this.props.stageOnly ? true : false,
+        },
+        ux: {
+          autoCreate: "true",
+        },
+        identity: {
+          name: this.props.identity?.displayName,
+          avatar: this.props.identity?.avatar?.uri,
+        },
+      })
+    );
+    let uri = `https://${this.props.advanced?.jamHost || "jam.systems"}/${
+      this.props.roomId
+    }#${iFrameHash}`;
 
-		console.log("editor");
-		console.log(this.props.editor);
-		console.log(this.props);
-
-		return <View/>;
-	}
+    return (
+      <View style={styles.wrapper}>
+        <WebView
+          source={{ uri }}
+          // width="100%"
+          // allow="microphone *;"
+          style={{ border: 0, height: 600 }}
+        ></WebView>
+      </View>
+    );
+  }
 }
 
-export default JamComponent
+const styles = StyleSheet.create({
+  wrapper: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default JamComponent;
